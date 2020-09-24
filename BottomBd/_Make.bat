0fe017565e076494b506c1dd6dd3f3d518cd9a87
zlkit <zlkit@piranti.co.kr>
2020-09-24 16:44:25 +0900

    [RFMBase] Library 소스 분리. / _Make.bat 빌드 후 이벤트 추가 및 바이너리 자동 채번.

--- BottomBd/_Make.bat
+++ BottomBd/_Make.bat
@@ -5,14 +5,16 @@ REM -- _Make.bat V1.3
 REM --
 REM -- Auto Module Make.
 REM -- _Make.bat [_Make.bat 상대경로]
-REM -- ex) _Make.bat ..\
+REM -- ex) _Make.bat ..\ [Debug/Release/Boot] [RFM/Bootloader]
 REM ------------------------------------------------------------------------------------
 
 REM -- 설정정보 얻기.
 rem call _Config.bat
 
 REM		인자가 있는경우 Skip
-if "%2" == "" (set MODULE=naranja_boron) else (set MODULE=%2)
+if "%2" == "" (set CONFIG=Release) else (set CONFIG=%2)
+
+if "%3" == "" (set MODULE=BottomBd) else (set MODULE=%3)
 
 REM -- 날짜 자동으로 넣어주기.
 for /F "tokens=1" %%a in ('date /t') do set DATE_TODAY=%%a
@@ -26,7 +28,7 @@ REM -- FileVersion 얻기.
 rem for /f "tokens=5 delims= " %%v in ('filever %EXE_FILE% /b') do set VER=%%v
 rem call %1_getVer.bat %1
 
-set shell_cmd=git rev-list HEAD --count .
+set shell_cmd=git rev-list HEAD --count %1
 FOR /F "tokens=*" %%F IN ('%shell_cmd%') DO (SET VER=%%F)
 
 echo %VER%
@@ -44,11 +46,11 @@ set BIN_FILE_NAME=RFMBase_%TODAY%_v1.%VER%.bin
 set HEX_FILE_NAME=RFMBase_%TODAY%_v1.%VER%.hex
 REM set BOOT_FILE_NAME=smrt7tni_bootloader_%TODAY%_v%VER%.hex
 
-echo copy "Release\%MODULE%.bin" "bin\%BIN_FILE_NAME%"
-copy "%1Release\%MODULE%.bin" "%1bin\%BIN_FILE_NAME%"
+echo copy "%CONFIG%\%MODULE%.bin" "bin\%BIN_FILE_NAME%"
+copy "%1\%CONFIG%\%MODULE%.bin" "%1\..\bin\%BIN_FILE_NAME%"
 
 echo copy "Release\%MODULE%.hex" "bin\%HEX_FILE_NAME%"
-copy "%1Release\%MODULE%.hex" "%1bin\%HEX_FILE_NAME%"
+copy "%1\%CONFIG%\%MODULE%.hex" "%1\..\bin\%HEX_FILE_NAME%"
 
 rem echo copy "MDK-ARM\smrt7tni_bootloader\smrt7tni_bootloader.hex" "bin\%BOOT_FILE_NAME%"
 rem copy ".\MDK-ARM\smrt7tni_bootloader\smrt7tni_bootloader.hex" ".\bin\%BOOT_FILE_NAME%"
